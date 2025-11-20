dcd87d72746ad3f2a780bf612994aba0b1946d85
Franco Gomez <69886027+FrancoGomez@users.noreply.github.com>
2025-11-20 03:00:51 +0000

    fix prisma + build + routes

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
-
-export const prisma =
-  globalForPrisma.prisma ??
+const prismaClientSingleton = () =>
   new PrismaClient({
-    datasourceUrl: process.env.DATABASE_URL,
-    log: ["query", "error", "warn", "info"],
+    log: ["query", "info", "warn", "error"],
   });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
+}
+
+export const prisma = global.prisma ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
